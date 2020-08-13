318deadf02adc0bd9331cb65f2b455b94f6c6334
ZaStack <stackzpersonal@gmail.com>
2020-08-13 12:17:06 -0700

    Renames seed files for successfull seeding

--- data/seeds/03-links.js
+++ data/seeds/03-links.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('links').del()
+  return knex('links').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('links').insert([
--- data/seeds/05-event_tags.js
+++ data/seeds/05-event_tags.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('event_tags').del()
+  return knex('event_tags').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('event_tags').insert([
