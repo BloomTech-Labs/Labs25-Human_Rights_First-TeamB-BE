6d056488ea1c17f5c701339ca99f7b658d489601
ZaStack <stackzpersonal@gmail.com>
2020-08-13 12:42:25 -0700

    Runs linter to apply Lambda Labs standards

--- data/seeds/02-geocode.js
+++ data/seeds/02-geocode.js
@@ -1,13 +1,13 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('geocode').truncate()
+  return knex('geocode')
+    .truncate()
     .then(function () {
       // Inserts seed entries
       return knex('geocode').insert([
-        {id: 1, city_id: 1, event_id: 1, lat: 45.523064, long: -122.676483},
-        {id: 2, city_id: 2, event_id: 2, lat: 47.608013, long: -122.335167},
-        {id: 3, city_id: 3, event_id: 3, lat: 37.804363, long: -122.271111}
+        { id: 1, city_id: 1, event_id: 1, lat: 45.523064, long: -122.676483 },
+        { id: 2, city_id: 2, event_id: 2, lat: 47.608013, long: -122.335167 },
+        { id: 3, city_id: 3, event_id: 3, lat: 37.804363, long: -122.271111 },
       ]);
     });
 };
--- data/seeds/05-event_tags.js
+++ data/seeds/05-event_tags.js
@@ -1,17 +1,17 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('event_tags').truncate()
+  return knex('event_tags')
+    .truncate()
     .then(function () {
       // Inserts seed entries
       return knex('event_tags').insert([
-        {event_id: 1, tag_id: 1},
-        {event_id: 1, tag_id: 2},
-        {event_id: 2, tag_id: 1},
-        {event_id: 2, tag_id: 3},
-        {event_id: 3, tag_id: 1},
-        {event_id: 3, tag_id: 2},
-        {event_id: 3, tag_id: 3},
+        { event_id: 1, tag_id: 1 },
+        { event_id: 1, tag_id: 2 },
+        { event_id: 2, tag_id: 1 },
+        { event_id: 2, tag_id: 3 },
+        { event_id: 3, tag_id: 1 },
+        { event_id: 3, tag_id: 2 },
+        { event_id: 3, tag_id: 3 },
       ]);
     });
 };
