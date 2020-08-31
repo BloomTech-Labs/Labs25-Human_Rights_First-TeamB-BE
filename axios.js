5a7169d9992aaf7eea00f87743a2ba63c66f3ef6
Keyeric <keyeric.bristol@gmail.com>
2020-08-31 08:24:58 -0700

    Monday update

--- axios.js
+++ axios.js
@@ -1,14 +1,31 @@
 const axios = require('axios');
 
-// Direct API Call
-axios
-  .get('https://api.846policebrutality.com/api/incidents')
-  .then((response) => {
-    console.log(response.data.data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+module.exports = {
+  axiFunc,
+};
+
+let data = [];
+function axiFunc() {
+  // Direct API Call
+  axios
+    .get('https://api.846policebrutality.com/api/incidents')
+    .then((response) => {
+      data = JSON.stringify(response.data.data);
+      // console.log('this is the then', data);
+      // return response.data.data;
+
+      // console.log(response.data.data);
+    })
+    .catch((error) => {
+      console.log('this is an error', error);
+      return 'This is broken';
+    });
+  // .finally(() => {
+  //   // console.log('this is the finally');
+  //   // console.log(data);
+  //   return data;
+  // })
+}
 
 // Data Science Call
 // axios
@@ -19,3 +36,5 @@ axios
 //   .catch((error) => {
 //     console.log(error);
 //   });
+
+console.log(data);
