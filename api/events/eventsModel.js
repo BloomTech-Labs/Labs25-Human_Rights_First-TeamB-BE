89d2cafe380dab5403612f3e358de3051979639d
Keyeric <keyeric.bristol@gmail.com>
2020-09-01 10:59:33 -0700

    Linter is a big baby

--- api/events/eventsModel.js
+++ api/events/eventsModel.js
@@ -5,13 +5,13 @@ const axios = require('axios');
 module.exports = {
   get,
 };
-
 function get() {
   axios
     .get(
       'http://hrf-teamb.eba-3253gq3h.us-east-1.elasticbeanstalk.com/getdata?pullnum=10'
     )
     .then((res) => {
+      console.log(db);
       console.log(res); // Data is available here!
       // From here Call your model and save the data into your database
     })
