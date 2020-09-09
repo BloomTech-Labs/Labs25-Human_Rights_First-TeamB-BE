3b66d9abe59327d767bfb36a23bdfdcf19ce0992
Keyeric <keyeric.bristol@gmail.com>
2020-09-09 12:25:14 -0700

    forgot to save a file

--- data/seeds/00-data.js
+++ data/seeds/00-data.js
@@ -1,8 +1,10 @@
 const file = require('../incidents.json');
 exports.seed = function (knex) {
+  // Deletes ALL existing entries
   return knex('data')
     .truncate()
     .then(function () {
+      // Inserts seed entries
       return knex('data').insert(getData());
     });
 };
