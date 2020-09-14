2cad9a134192b61e31cf0813fa3a9b2f60efc50b
Keyeric <keyeric.bristol@gmail.com>
2020-09-14 14:08:04 -0700

    Attempt 4f2

--- axios.js
+++ axios.js
@@ -1,38 +1,38 @@
 const axios = require('axios');
-const db = require('./data/db-config');
-const model = require("./api/events/eventsModel")
+// const db = require('./data/db-config');
+const model = require('./api/events/eventsModel');
 
 function axiData() {
   axios
-  .get(
-    'http://hrf-teamb.eba-3253gq3h.us-east-1.elasticbeanstalk.com/getdata?pullnum=10'
-  )
-  .then((res) => {
-    //   sci is arr
-    var sci = JSON.parse(res.data);
-    // console.log(sci)
-    //datum is obj
-    for (i in sci){
+    .get(
+      'http://hrf-teamb.eba-3253gq3h.us-east-1.elasticbeanstalk.com/getdata?pullnum=10'
+    )
+    .then((res) => {
+      //   sci is arr
+      var sci = JSON.parse(res.data);
+      // console.log(sci)
+      //datum is obj
+      for (let i in sci) {
         // console.log("yay!", sci[i])
-        eventObj = {
-            eventId: sci[i].id,
-            event: sci[i]
-        }
+        var eventObj = {
+          eventId: sci[i].id,
+          event: sci[i],
+        };
         // console.log("boo!", eventObj)
-        model.add(eventObj)
+        model.add(eventObj);
         // console.log("three!!", eventObj)
 
-    // testObj = {
-    //     eventId: "test",
-    //     event: {
-    //         id: "test",
-    //         test: "value"}
-        }
-        // let returnVal = await model.add(testObj)
-        // console.log(returnVal)
-  })
-  .catch((err) => {
-    console.log(err);
-  })
+        // testObj = {
+        //     eventId: "test",
+        //     event: {
+        //         id: "test",
+        //         test: "value"}
+      }
+      // let returnVal = await model.add(testObj)
+      // console.log(returnVal)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 axiData();
