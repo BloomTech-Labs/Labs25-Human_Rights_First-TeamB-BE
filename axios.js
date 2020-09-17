7fa8592aec24d6acba88abecbcfa9291ab8f737e
Keyeric <keyeric.bristol@gmail.com>
2020-09-16 22:28:35 -0700

    Attempting to fix executuion hang

--- axios.js
+++ axios.js
@@ -5,13 +5,19 @@ function axiData() {
   axios
     .get('http://hrf-teamb.eba-3253gq3h.us-east-1.elasticbeanstalk.com/getdata')
     .then(async (res) => {
-      var sci = JSON.parse(res.data);
-      for (let i in sci) {
-        var eventObj = {
-          eventId: sci[i].id,
-          event: sci[i],
-        };
-        model.add(eventObj);
+      if (res.data == '[]') {
+        console.log('No New Data');
+      } else {
+        console.log(res.data);
+        var sci = JSON.parse(res.data);
+        for (let i in sci) {
+          var eventObj = {
+            eventId: sci[i].id,
+            event: sci[i],
+          };
+          await model.add(eventObj);
+          console.log('model.add exited');
+        }
       }
     })
     .catch((err) => {
